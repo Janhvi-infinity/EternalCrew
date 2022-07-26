a29a8081184eb45659e2d9cd158b2551ef4f9d34
Ashhar909 <ashhar374@gmail.com>
2022-07-26 13:49:24 +0530

    nothing much

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -94,7 +94,7 @@ exports.exploreProject = async(req, res) => {
     try {
         let searchTerm = req.body.searchTerm;
         let project = await Project.find( { $text: { $search: searchTerm, $diacriticSensitive: true}} );
-        res.render('search' , {title: 'Projecto-Search' ,project  });
+        // res.render('search' , {title: 'Projecto-Search' ,project  });
         res.json(project);
     } catch (error) {
         res.send({message: error.message || "Error Occured" });
@@ -109,7 +109,8 @@ exports.exploreLatest = async(req, res) => {
     try {
         const limitNumber = 20;
         const project = await Project.find({}).sort({ _id: -1 }).limit(limitNumber);
-        res.render('explore-latest', { title: 'Cooking Blog - Explore Latest', project } );
+        // res.render('explore-latest', { title: 'Cooking Blog - Explore Latest', project } );
+        res.json(project);
       } catch (error) {
         res.satus(500).send({message: error.message || "Error Occured" });
       }
